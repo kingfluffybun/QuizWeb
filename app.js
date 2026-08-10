f389f3e105e446199a7aae3c2afd9c682cb88890
GaZeRnG <gazeringg@gmail.com>
2026-08-10 15:04:14 +0800

    fixed ejs not showing correct pages and styles

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const app = express();
 app.set('view engine', 'ejs');
 
 // Serve static assets from the "public" folder
-app.use(express.static('public')); 
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use(express.static('public'), express.static('views')); 
 
 app.listen(3000, () => console.log('Server running on port 3000'));
