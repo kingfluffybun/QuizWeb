f4150e482e21e78d9a7c513b32241f81de835b21
Carl Adrian Tan <catan@udm.edu.ph>
2026-08-15 17:08:59 +0800

    Added About Layout

---
+++ app/about/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Image from "next/image";
+import "#css/landing-page.css";
+
+export default function Home() {
+    return (
+        <body>
+            <section className="hero">
+                <nav>
+                    <div><a href="test.com"><p>Logo</p></a></div>
+                    <div><a href="/quiz/index.html"><p>Learn</p></a></div>
+                    <div><a href="test.com"><p>Leaderboard</p></a></div>
+                    <div><a href="test.com"><p>About</p></a></div>
+                    <div><a href="/login"><p>Log in</p></a></div>
+                    <div><a href="/login"><p>Sign Up</p></a></div>
+                </nav>
+            </section>
+        </body>
+    )
+}
