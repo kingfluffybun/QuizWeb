e2864a1acb6465b0ebff47c2617b1d8bd15b005a
Fluffybun <Clarence.luna651@gmail.com>
2026-08-16 12:17:14 +0800

    small fix sa landing page

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,12 @@ export default function Home() {
             <section className="hero">
                 <nav>
                     <div><a href="test.com"><p>Logo</p></a></div>
-                    <button className="menu-btn">
+                    <button className="menu-btn"> {/* for mobile 'to */}
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu-icon lucide-menu"><path d="M4 5h16"/><path d="M4 12h16"/><path d="M4 19h16"/></svg>
                         <p>Menu</p>
                     </button>
                     <div className="nav-options">
-                        <div><a href="/quiz/index.html"><p>Learn</p></a></div>
+                        <div><a href="/quiz"><p>Learn</p></a></div>
                         <div><a href="test.com"><p>Leaderboard</p></a></div>
                         <div><a href="/about"><p>About</p></a></div>
                         <div><a href="/login"><p>Log in</p></a></div>
