c0ed6b61e5cd949253f67962ed8d3bca98ea96ee
Fluffybun <Clarence.luna651@gmail.com>
2026-09-07 01:45:31 +0800

    inayos yung cta button

--- app/page.tsx
+++ app/page.tsx
@@ -35,8 +35,11 @@ export default function Home() {
                 <h1>Writing Code gets easier, QuizWeb makes it stick.</h1>
                 <p>Earn daily streaks, unlock new levels, and learn HTML, CSS, and JavaScript the fun way.</p>
                 <div className="call-to-action row">
-                    <h1>Start Learning</h1>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
+                    <h2>Start Learning</h2>
+                    <div className="call-to-action-icon col">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
+                    </div>
+                    
                 </div>
             </div>
             
