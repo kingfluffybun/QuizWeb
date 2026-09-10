1d4eb19b936e2982c40ed608d8ac771ab56f06c1
Fluffybun <Clarence.luna651@gmail.com>
2026-09-10 22:18:10 +0800

    bug fixes sa rendering ng quiz

--- app/components/quiz/Order.tsx
+++ app/components/quiz/Order.tsx
@@ -88,6 +88,7 @@ export default function Order ({ quiz, value, onChange }: QuestionProps) {
                             onDrop={() => handleDrop(index)}
                             onDragEnd={() => { setDraggedIdx(null); setDragOverIdx(null); }}
                         >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-grip-vertical"><circle cx="9" cy="12" r="1"/><circle cx="9" cy="5" r="1"/><circle cx="9" cy="19" r="1"/><circle cx="15" cy="12" r="1"/><circle cx="15" cy="5" r="1"/><circle cx="15" cy="19" r="1"/></svg>
                             <p>{item}</p>
                         </div>
                     );
