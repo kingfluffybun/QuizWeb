5d5584bd7964ed2de8008127df23360cc7caf0a2
GaZeRnG <gazeringg@gmail.com>
2026-09-12 12:08:40 +0800

    fix

--- app/verify-email/[token]/page.tsx
+++ app/verify-email/[token]/page.tsx
@@ -38,7 +38,7 @@ export default async function VerifyEmailPage({
                 ) : (
                     <>
                         <div className="failed-logo">
-                            <Image className="logo-img" src="/assets/QuizWeb-Logo.svg" width={96} height={96} alt="" />
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-badge-alert"><path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/><line x1="12" x2="12" y1="8" y2="12"/><line x1="12" x2="12.01" y1="16" y2="16"/></svg>
                         </div>
 
                         <h1 className="failed-title">
