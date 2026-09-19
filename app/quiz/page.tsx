46153bcf584340e4d7502c699d3749f17390e843
Fluffybun <Clarence.luna651@gmail.com>
2026-09-19 19:20:18 +0800

    fixed sidebar

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -167,7 +167,7 @@ export default function QuizPage() {
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-panel-left"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="M9 3v18"/></svg>
                     </button>
-                    <div className="sidebar-option">
+                    <div className="sidebar-option active">
                         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-book-minus"><path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20"/><path d="M9 10h6"/></svg>
                         <p>Learn</p>
                     </div>
